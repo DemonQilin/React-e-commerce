9c501a54a88946c9ed8f3e38f1760ac7aa69a362
DemonQilin <velezloaizaesteban9012015@gmail.com>
2022-07-09 20:28:50 -0500

    Se crearon los demás thunks correspondientes para añadir, actualizar y borrar un producto del carrito

--- src/store/slices/cart.slice.js
+++ src/store/slices/cart.slice.js
@@ -4,6 +4,16 @@ import getConfigure from "../../utils/getConfigure";
 import { loadingFalse, loadingTrue } from "./loading.slice";
 
 const url = 'https://ecommerce-api-react.herokuapp.com/api/v1/cart';
+const managmentError = error => {
+    if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+    } else {
+        console.log('Error', error.message);
+    }
+    console.log('Review config', error.config);
+    dispatch(loadingFalse());
+};
 
 const cartSlice = createSlice({
     name: "cart",
@@ -31,7 +41,39 @@ export const getCart = () => async dispatch => {
         };
         console.log('Review config', error.config);
     } finally { dispatch(loadingFalse()) }
-}
+};
+
+export const addProductCart = (id, quantity) => async dispatch => {
+    dispatch(loadingTrue());
+    try {
+        const res = await axios.post(url, { id, quantity }, getConfigure()).then(res => res.data.status);
+
+        if (res === "success") dispatch(getCart());
+    } catch (error) {
+        managmentError(error);
+    }
+};
+
+export const updateProductCart = (id, newQuantity) => async dispatch => {
+    dispatch(loadingTrue());
+    try {
+        const res = await axios.patch(url, { id, newQuantity }, getConfigure());
+
+        if (res.status === 204) dispatch(getCart());
+    } catch (error) {
+        managmentError(error);
+    }
+};
+
+export const deleteProductCart = id => async dispatch => {
+    dispatch(loadingTrue());
+    try {
+        const res = await axios.delete(`${url}/${id}`, getConfigure());
+        if (res.status === 204) dispatch(getCart());
+    } catch (error) {
+        managmentError(error);
+    }
+};
 
 export const { setGlobalCart } = cartSlice.actions;
 
